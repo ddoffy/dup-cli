053cc3f19152794cbae06dff9f5b4838f44e8dac
D. Doffy <cuongnsm@gmail.com>
2025-02-22 23:51:09 -0600

    Add total time and total size and average speed of upload

--- src/main.rs
+++ src/main.rs
@@ -23,24 +23,53 @@ async fn main() {
         }
     }
 
+    // create a channel to sum total file size
+    let (tx, rx) = std::sync::mpsc::channel();
+
     let mut handles = vec![];
 
     for path in paths {
         let url_clone = args.host.clone();
         let category = args.category.clone();
+        let tx_clone = tx.clone();
         let handle = std::thread::spawn(move || {
             let runtime = tokio::runtime::Runtime::new().unwrap();
 
             runtime.block_on(async {
-                let _ = handle_upload_file(path, &url_clone, category).await;
+                let _ = handle_upload_file(path, &url_clone, category, tx_clone).await;
             });
         });
         handles.push(handle);
     }
 
+    let total_time = std::time::Instant::now();
+
     for handle in handles {
         handle.join().unwrap();
     }
+
+    let total_time = total_time.elapsed().as_secs();
+
+    println!("Total time: {}s", total_time);
+
+    let mut total_size = 0;
+    // drain the channel to sum total file size
+    loop {
+        match rx.try_recv() {
+            Ok(size) => {
+                total_size += size;
+            }
+            Err(_) => {
+                break;
+            }
+        }
+    }
+
+    println!("Total size: {}", file_size_human_readable(total_size));
+
+    // the average speed of the upload
+    let average_speed = total_size as f64 / total_time as f64;
+    println!("Average speed: {}/s", file_size_human_readable(average_speed as u64));
 }
 
 fn handle_path(path: std::path::PathBuf, paths: &mut Vec<std::path::PathBuf>) {
@@ -53,13 +82,32 @@ fn handle_path(path: std::path::PathBuf, paths: &mut Vec<std::path::PathBuf>) {
     }
 }
 
+fn file_size_human_readable(file_size: u64) -> String {
+    let file_size = if file_size <= 1024 {
+        format!("{}B", file_size)
+    } else if file_size <= 1024 * 1024 {
+        format!("{:.2}KB", file_size as f64 / 1024.0)
+    } else if file_size <= 1024 * 1024 * 1024 {
+        format!("{:.2}MB", file_size as f64 / 1024.0 / 1024.0)
+    } else {
+        format!("{:.2}GB", file_size as f64 / 1024.0 / 1024.0 / 1024.0)
+    };
+    file_size
+}
+
 async fn handle_upload_file(
     path: std::path::PathBuf,
     url: &str,
     kind_of_upload: uploader::KindOfUpload,
+    tx: std::sync::mpsc::Sender<u64>,
 )  -> Result<(), Box<dyn std::error::Error>> {
+    let time = std::time::Instant::now();
     let mut uploader = Uploader::new(url);
-    println!("Starting upload of {}", path.display());
+    let file_size = std::fs::metadata(&path)?.len();
+    tx.send(file_size).unwrap();
+    let file_size = file_size_human_readable(file_size);
+        
+    println!("Starting upload of {} [{}]", path.display(), file_size);
 
     if kind_of_upload == uploader::KindOfUpload::Binary {
         match uploader.add_header(
@@ -80,17 +128,20 @@ async fn handle_upload_file(
 
     match uploader.upload_file(&path).await {
         Ok(res) => {
-            println!("Upload of {} successful", path.display());
-            // check if res is json, text print it
-            match res.text().await {
+
+            let download_link = match res.text().await {
                 Ok(text) => {
-                    println!("You can download with this link: {}", text);
+                    text
                 }
-                Err(e) => eprintln!("Error: {}", e),
-            }
+                Err(_) => {
+                    "Error".to_string()
+                }
+            };
+            println!("[{}s][{}][{}] - Doanload: {}", time.elapsed().as_secs(), path.display(), file_size, download_link);
+            // check if res is json, text print it
         }
         Err(e) => {
-            eprintln!("Error: {}", e);
+            eprintln!("[{}s] Error: {}", time.elapsed().as_secs(), e);
             return Err(e);
         }
 
